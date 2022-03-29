7bc0d9d883a085f806b6dbb82e8b60ead10f465e
vinialves-de <81194840+vinialves-de@users.noreply.github.com>
2022-03-29 16:39:25 -0300

    Update const.php

--- model/const.php
+++ model/const.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_HOST','vinialves.mysql.database.azure.com');
-define('DB_USER','vini@vinialves');
-define('DB_PASS'@@lobin1213'');
-define('DB_NAME','hortfrutbd');
+define('DB_HOST','viniciusalves.mysql.database.azure.com');
+define('DB_USER','vini@viniciusalves');
+define('DB_PASS','@@lobin1213');
+define('DB_NAME','hortfruitbd');
